cf3e2f034bdc827bdaee7f594e0661b116ab6ff5
Nirmalkanniaiyan <104248259+Nirmalkanniaiyan@users.noreply.github.com>
2023-08-19 17:54:08 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,9 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51MZuuRSBtHjplQmuungjoebOTonRs7XBvWJRSuwpVcDpGFzBktf8jm7OtGU7aDiPHP9XVS0GWc3ow9QB4zpdVomN00ylJkTMfq"
+  ""
 );
+// Enter your stripe API key 
 
 // app.post("/payments/create", async (req, res) => {
 //   const total = req.query.total; //this will be in sub currencies
@@ -57,4 +58,4 @@ exports.api = functions.https.onRequest(app);
 
 // // this is the set up needed to get the backend express app running on a cloud function
 
-// // API end point(here is where the API is running) -- http://127.0.0.1:5001/clone-3ccac/us-central1/api
+// // API end point(here is where the API is running) --
